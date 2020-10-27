b45e6553e33bf741174dbbd6972870cccf9039a4
Matt-1985 <blatsche@web.de>
2020-10-27 17:03:37 +0100

    Add fixes

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,33 +1,39 @@
 import "./board.css";
 import React, { useState } from "react";
 import Square from "./Square";
+import calculateWinner from "../utils/GameEngine";
+// import { getRandomPlayer } from "../utils/GameEngine"
 
 export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null)); // hier füllen wir unser array mit 9 x null
+  const [xIsNext, setXIsNext] = useState("🍆");
+
+  const winner = calculateWinner(squares);
 
   function handleClick(index) {
-    if (calculateWinner(squares) || squares[index]) {
+    if (winner) {
       return;
     }
 
-    const newSquares = squares.slice();
-    newSquares[index] = xIsNext ? "🍆" : "🍑";
+    const newSquares = squares.slice(); // .slice erstllt eine kopie vom array (immer wenn sie im state drin sind)
+    newSquares[index] = xIsNext;
     setSquares(newSquares);
-    setXIsNext(!xIsNext);
+    setXIsNext(xIsNext === "🍆" ? "🍑" : "🍆");
   }
 
   function renderSquare(index) {
     return <Square value={squares[index]} onClick={() => handleClick(index)} />;
   }
 
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "🍆" : "🍑");
-  }
+  // const winner = calculateWinner(squares);
+  // let status;
+  // if (winner) {
+  //   status = "Winner: " + winner;
+  // } else {
+  //   status = "Next player: " + (xIsNext ? "🍆" : "🍑");
+  // }
+
+  const status = winner ? `Winner: ${winner}` : `Next Player: ${xIsNext}`;
 
   return (
     <div>
@@ -49,28 +55,24 @@ export default function Board() {
       </div>
     </div>
   );
-
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let index = 0; index < lines.length; index++) {
-      const [a, b, c] = lines[index];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
 }
+
+// function calculateWinner(squares) {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ];
+//   for (let index = 0; index < lines.length; index++) {
+//     const [a, b, c] = lines[index];
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       return squares[a];
+//     }
+//   }
+//   return null;
+// }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,5 @@
 import "./square.css";
-import React, { useState } from "react";
+import React from "react";
 
 export default function Square(props) {
   return (
